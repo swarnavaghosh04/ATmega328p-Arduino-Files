ba126473caa5fd3ce030d0c11d9ed0417eced69e
Swarnava Ghosh <swarnavaghosh04@gmail.com>
2020-08-16 22:16:50 -0400

    c

--- src/Monody.c
+++ src/Monody.c
@@ -2,7 +2,7 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #define BPM 100
-#include "music.h" // !!!!!!!!!!!!!!
+#include "..\\headers\\music.h" // !!!!!!!!
 
 #ifndef F_CPU
 #define F_CPU 16000000UL
